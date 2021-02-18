eb83a258357c5bd9e7ede90a661790eea3f9f0bc
PRATIK-CHOLKE <62254051+PRATIK-CHOLKE@users.noreply.github.com>
2021-02-18 17:08:59 +0530

    After Adding CORS

--- Crud Api using Student DB/app.js
+++ Crud Api using Student DB/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const url = 'mongodb://localhost/StudDB';
 
 const app = express();
@@ -8,11 +9,26 @@ let port = process.env.PORT || 9000;
 mongoose.connect(process.env.MONGODB_URI || url, {
   useNewUrlParser: true
 });
+
 const con = mongoose.connection;
 
 con.on('open', () => console.log('Connected...'));
 
 app.use(express.json())
+
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const studentRouter = require('./routers/students');
 app.use('/students', studentRouter);
